2b2c314c20a51571e07e47174d8dff92b0bc0c59
Matthew <alexandermatthew12@gmail.com>
2022-12-27 01:15:46 +0700

    Add new test case to completely migrate positions

--- test/FlashLoanV2.t.sol
+++ test/FlashLoanV2.t.sol
@@ -8,9 +8,9 @@ import "src/FlashLoanV2.sol";
 
 import { DataTypes } from "src/libraries/DataTypes.sol";
 import { IDebtToken } from "src/interfaces/IDebtToken.sol";
-import { IProtocolDataProviderV2 } from "src/interfaces/IProtocolDataproviderV2.sol";
+import { IProtocolDataProviderV2 } from "src/interfaces/IProtocolDataProviderV2.sol";
 
-
+// BLOCK_NUMBER = 16232570 -> mainnet fork (testing)
 contract FlashLoanV2Test is Test {
     FlashLoanV2 flashloan;
 
@@ -22,32 +22,39 @@ contract FlashLoanV2Test is Test {
     address private constant USDC_ADDRESS = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
     address private constant STABLE_DEBT_USDC = 0xE4922afAB0BbaDd8ab2a88E0C79d884Ad337fcA6;
     address private constant VARIABLE_DEBT_USDC = 0x619beb58998eD2278e08620f97007e1116D5D25b;
+    
+    address private constant ST_ETH_ADDRESS = 0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84;
+    address private constant A_ST_ETH_ADDRESS = 0x1982b2F5814301d4e9a8b0201555376e62F82428;
 
     // Collateral
     address private constant A_DAI_ADDRESS = 0x028171bCA77440897B824Ca71D1c56caC55b68A3;
     // Collateral (underlying)
     address private constant DAI_ADDRESS = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
 
-    
-    // https://etherscan.io/tx/0xb631bbbfa79b3f09eaa32a2e72e13c78aaac9880798bb50137baef7057d98b4c
-    address private constant BORROWER = 0xb6aF7C04f67B5eb61F0DC7aC4a760888EC3E3887;
+    IERC20 usdcToken = IERC20(USDC_ADDRESS);
+    IERC20 aDaiToken = IERC20(A_DAI_ADDRESS);
+    IERC20 stEthToken = IERC20(ST_ETH_ADDRESS);
+    IERC20 aStEthToken = IERC20(A_ST_ETH_ADDRESS);
 
     // No positions on Aave yet
     address private constant RECEIVER = 0x716Abf4d1e0FE3335629C43adA2680Da5BeE67be;
-
-    IERC20 usdcToken = IERC20(USDC_ADDRESS);
-    IERC20 aDaiToken = IERC20(A_DAI_ADDRESS);
+    address private migrateAavePositionsAddress;
 
     IProtocolDataProviderV2 protocolDataProvider = IProtocolDataProviderV2(AAVE_PROTOCOL_DATA_PROVIDER);
 
     function setUp() public {
         flashloan = new FlashLoanV2(AAVE_LENDING_POOL_PROVIDER);
-        vm.label(BORROWER, "Borrower");
+        migrateAavePositionsAddress = flashloan.getContractAddress();
         vm.label(RECEIVER, "Receiver");
     }
 
-    function test_migrateUSDCPosition() public {
-        address migrateAavePositionsAddress = flashloan.getContractAddress();
+
+
+    function test_migrateUsdcPositionsWithDaiCollateral() public {
+        // https://etherscan.io/tx/0xb631bbbfa79b3f09eaa32a2e72e13c78aaac9880798bb50137baef7057d98b4c
+        address BORROWER = 0xb6aF7C04f67B5eb61F0DC7aC4a760888EC3E3887;
+        vm.label(BORROWER, "Borrower");
+
 
         vm.startPrank(BORROWER); 
         DataTypes.DebtTokenPosition[] memory debtTokenPositions = new DataTypes.DebtTokenPosition[](1);
@@ -108,13 +115,57 @@ contract FlashLoanV2Test is Test {
         assertEq(usdcReceiverVariableDebt, amountUsdcToTransfer + flashloanFee);
     }
 
+    // https://etherscan.io/address/0xeaf1890cac871f93c15009b7a05e2e5e076911e1?toaddress=0x7d2768de32b0b80b7a3454c06bdac94a69ddc7a9
+    function test_migrateUsdcPositionsWithEthCollateral() public {
+        address BORROWER = 0xEAf1890cAC871F93c15009b7A05e2E5E076911e1;
+        vm.label(BORROWER, "Borrower");
 
-    function testBar() public {
-        assertEq(uint256(1), uint256(1), "ok");
-    }
+        vm.startPrank(BORROWER); 
+        DataTypes.DebtTokenPosition[] memory debtTokenPositions = new DataTypes.DebtTokenPosition[](1);
+        DataTypes.aTokenPosition[] memory aTokenPositions = new DataTypes.aTokenPosition[](1);
 
-    function testFoo(uint256 x) public {
-        vm.assume(x < type(uint128).max);
-        assertEq(x + x, x * 2);
-    }
-}
+        uint256 aStEthTokenAmount = aStEthToken.balanceOf(BORROWER);
+        (, , uint usdcBorrowerVariableDebt, , uint usdcBorrowerScaledVariableDebt, , , ,) = protocolDataProvider.getUserReserveData(USDC_ADDRESS, BORROWER);
+
+        aTokenPositions[0] = DataTypes.aTokenPosition({
+            tokenAddress: ST_ETH_ADDRESS,
+            aTokenAddress: A_ST_ETH_ADDRESS,
+            amount: aStEthTokenAmount
+        });
+
+        debtTokenPositions[0] = DataTypes.DebtTokenPosition({
+            stableDebtAmount: 0, 
+            // Give buffer for flash loan fee (0.09%) -> 0.1% buffer
+            variableDebtAmount: usdcBorrowerVariableDebt, 
+            tokenAddress: USDC_ADDRESS
+        });
+        
+        // Pre-approve aToken positions transfer on "sender" wallet
+        IERC20(A_ST_ETH_ADDRESS).approve(
+            migrateAavePositionsAddress, 
+            type(uint256).max
+        );
+        vm.stopPrank();
+
+        vm.startPrank(RECEIVER);
+        // Pre-approve borrow positions on "RECEIVER" wallet
+        IDebtToken(VARIABLE_DEBT_USDC).approveDelegation(
+            migrateAavePositionsAddress, 
+            usdcBorrowerVariableDebt
+        );
+        IDebtToken(STABLE_DEBT_USDC).approveDelegation(
+            migrateAavePositionsAddress, 
+            usdcBorrowerVariableDebt
+        );
+        vm.stopPrank();
+
+        // Start flash loan
+        vm.startPrank(BORROWER);
+        flashloan.migrateAavePositions(
+            RECEIVER, 
+            debtTokenPositions, 
+            aTokenPositions
+        );
+        vm.stopPrank();
+    }    
+}   
