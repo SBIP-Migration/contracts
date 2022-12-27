4a745130257f846c2ee11328f35d5fa7436ad70b
Matthew <alexandermatthew12@gmail.com>
2022-12-27 23:37:59 +0700

    Add more tests for multiple lending positions

--- test/FlashLoanV2.t.sol
+++ test/FlashLoanV2.t.sol
@@ -27,18 +27,21 @@ contract FlashLoanV2Test is Test {
     address private constant A_ST_ETH_ADDRESS = 0x1982b2F5814301d4e9a8b0201555376e62F82428;
     
     address private constant WETH_ADDRESS = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address private constant A_WETH_ADDRESS = 0x030bA81f1c18d280636F32af80b9AAd02Cf0854e;
     address private constant VARIABLE_DEBT_WETH = 0xF63B34710400CAd3e044cFfDcAb00a0f32E33eCf;
+
+    address private constant WBTC_ADDRESS  = 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599;
+    address private constant A_WBTC_ADDRESS = 0x9ff58f4fFB29fA2266Ab25e75e2A8b3503311656;
+
+    address private constant SNX_ADDRESS = 0xC011a73ee8576Fb46F5E1c5751cA3B9Fe0af2a6F;
+    address private constant A_SNX_ADDRESS = 0x35f6B052C598d933D69A4EEC4D04c73A191fE6c2;
 
     // Collateral
     address private constant A_DAI_ADDRESS = 0x028171bCA77440897B824Ca71D1c56caC55b68A3;
     // Collateral (underlying)
     address private constant DAI_ADDRESS = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
 
-    IERC20 usdcToken = IERC20(USDC_ADDRESS);
-    IERC20 aDaiToken = IERC20(A_DAI_ADDRESS);
-    IERC20 stEthToken = IERC20(ST_ETH_ADDRESS);
     IERC20 aStEthToken = IERC20(A_ST_ETH_ADDRESS);
-    IERC20 wEthToken = IERC20(WETH_ADDRESS);
 
     // No positions on Aave yet
     address private constant RECEIVER = 0x716Abf4d1e0FE3335629C43adA2680Da5BeE67be;
@@ -67,9 +70,6 @@ contract FlashLoanV2Test is Test {
         (, , uint256 WETH_BORROWED, , , , , ,) = protocolDataProvider.getUserReserveData(WETH_ADDRESS, BORROWER); // 7.49 WETH
         (uint256 DAI_LENDED, , , , , , , ,) = protocolDataProvider.getUserReserveData(DAI_ADDRESS, BORROWER);
 
-        // TODO: Why we can't transfer all of the aDai lending positions?
-        uint256 amountDaiToTransfer = DAI_LENDED * 99 / 100;
-
         // USDC debt
         debtTokenPositions[0] = DataTypes.DebtTokenPosition({
             stableDebtAmount: 0, 
@@ -88,7 +88,7 @@ contract FlashLoanV2Test is Test {
         aTokenPositions[0] = DataTypes.aTokenPosition({
             tokenAddress: DAI_ADDRESS,
             aTokenAddress: A_DAI_ADDRESS,
-            amount: amountDaiToTransfer
+            amount: DAI_LENDED
         });
 
         // Pre-approve aToken positions transfer on "sender" wallet
@@ -119,11 +119,8 @@ contract FlashLoanV2Test is Test {
         // Borrower related
         (uint256 daiBorrowerATokenBalance, , , , , , , ,) = protocolDataProvider.getUserReserveData(DAI_ADDRESS, BORROWER);
 
-        // Rounding errors with "-1"
-        assertEq(daiBorrowerATokenBalance, DAI_LENDED - amountDaiToTransfer - 1);
         // Lending positions transferred to RECEIVER account
-        assertEq(amountDaiToTransfer, daiReceiverATokenBalance);
-        console.log(daiBorrowerATokenBalance);
+        assertEq(DAI_LENDED, daiReceiverATokenBalance);
 
         // 0.09% = 9 / 10000
         uint256 flashloanFeeUsdc = USDC_BORROWED * 9 / 10000;
@@ -142,19 +139,19 @@ contract FlashLoanV2Test is Test {
         DataTypes.DebtTokenPosition[] memory debtTokenPositions = new DataTypes.DebtTokenPosition[](1);
         DataTypes.aTokenPosition[] memory aTokenPositions = new DataTypes.aTokenPosition[](1);
 
-        uint256 aStEthTokenAmount = aStEthToken.balanceOf(BORROWER);
-        (, , uint usdcBorrowerVariableDebt, , , , , ,) = protocolDataProvider.getUserReserveData(USDC_ADDRESS, BORROWER);
+        (uint ST_ETH_LENDED, , , , , , , ,) = protocolDataProvider.getUserReserveData(ST_ETH_ADDRESS, BORROWER);
+        (, , uint USDC_BORROWED, , , , , ,) = protocolDataProvider.getUserReserveData(USDC_ADDRESS, BORROWER);
 
         aTokenPositions[0] = DataTypes.aTokenPosition({
             tokenAddress: ST_ETH_ADDRESS,
             aTokenAddress: A_ST_ETH_ADDRESS,
-            amount: aStEthTokenAmount
+            amount: ST_ETH_LENDED
         });
 
         debtTokenPositions[0] = DataTypes.DebtTokenPosition({
             stableDebtAmount: 0, 
             // Give buffer for flash loan fee (0.09%) -> 0.1% buffer
-            variableDebtAmount: usdcBorrowerVariableDebt, 
+            variableDebtAmount: USDC_BORROWED, 
             tokenAddress: USDC_ADDRESS
         });
         
@@ -169,11 +166,11 @@ contract FlashLoanV2Test is Test {
         // Pre-approve borrow positions on "RECEIVER" wallet
         IDebtToken(VARIABLE_DEBT_USDC).approveDelegation(
             migrateAavePositionsAddress, 
-            usdcBorrowerVariableDebt
+            USDC_BORROWED
         );
         IDebtToken(STABLE_DEBT_USDC).approveDelegation(
             migrateAavePositionsAddress, 
-            usdcBorrowerVariableDebt
+            USDC_BORROWED
         );
         vm.stopPrank();
 
@@ -190,9 +187,98 @@ contract FlashLoanV2Test is Test {
         (uint256 stEthReceiverATokenBalance, , , , , , , ,) = protocolDataProvider.getUserReserveData(ST_ETH_ADDRESS, RECEIVER);
 
         // Rounding errors with "-1"
-        assertEq(stEthReceiverATokenBalance, aStEthTokenAmount - 1);
+        assertEq(stEthReceiverATokenBalance, ST_ETH_LENDED - 1);
 
-        uint256 flashloanFee = usdcBorrowerVariableDebt * 9 / 10000;
-        assertEq(usdcReceiverVariableDebt, usdcBorrowerVariableDebt + flashloanFee);
+        uint256 flashloanFee = USDC_BORROWED * 9 / 10000;
+        assertEq(usdcReceiverVariableDebt, USDC_BORROWED + flashloanFee);
     }    
+
+    // Multiple lending positions
+    function testMigrateMultipleLendingPositions() public {
+        address BORROWER = 0x0B5a6a15B975fD35f0B301748C8DaBD35b50d8C5;
+        vm.label(BORROWER, "Borrower");
+
+        vm.startPrank(BORROWER); 
+        DataTypes.DebtTokenPosition[] memory debtTokenPositions = new DataTypes.DebtTokenPosition[](1);
+        DataTypes.aTokenPosition[] memory aTokenPositions = new DataTypes.aTokenPosition[](3);
+
+        (uint WETH_LENDED, , , , , , , ,) = protocolDataProvider.getUserReserveData(WETH_ADDRESS, BORROWER);
+        (uint WBTC_LENDED, , , , , , , ,) = protocolDataProvider.getUserReserveData(WBTC_ADDRESS, BORROWER);
+        (uint SNX_LENDED, , , , , , , ,) = protocolDataProvider.getUserReserveData(SNX_ADDRESS, BORROWER);
+
+        (, , uint USDC_BORROWED, , , , , ,) = protocolDataProvider.getUserReserveData(USDC_ADDRESS, BORROWER);
+
+        aTokenPositions[0] = DataTypes.aTokenPosition({
+            tokenAddress: WETH_ADDRESS,
+            aTokenAddress: A_WETH_ADDRESS,
+            amount: WETH_LENDED
+        });
+        
+        aTokenPositions[1] = DataTypes.aTokenPosition({
+            tokenAddress: WBTC_ADDRESS,
+            aTokenAddress: A_WBTC_ADDRESS,
+            amount: WBTC_LENDED
+        });
+        
+        aTokenPositions[2] = DataTypes.aTokenPosition({
+            tokenAddress: SNX_ADDRESS,
+            aTokenAddress: A_SNX_ADDRESS,
+            amount: SNX_LENDED
+        });
+
+        debtTokenPositions[0] = DataTypes.DebtTokenPosition({
+            stableDebtAmount: 0, 
+            // Give buffer for flash loan fee (0.09%) -> 0.1% buffer
+            variableDebtAmount: USDC_BORROWED, 
+            tokenAddress: USDC_ADDRESS
+        });
+        
+        // Pre-approve aToken positions transfer on "sender" wallet
+        IERC20(A_WETH_ADDRESS).approve(
+            migrateAavePositionsAddress, 
+            type(uint256).max
+        );
+        
+        IERC20(A_WBTC_ADDRESS).approve(
+            migrateAavePositionsAddress, 
+            type(uint256).max
+        );
+        
+        IERC20(A_SNX_ADDRESS).approve(
+            migrateAavePositionsAddress, 
+            type(uint256).max
+        );
+        vm.stopPrank();
+
+        vm.startPrank(RECEIVER);
+        // Pre-approve borrow positions on "RECEIVER" wallet
+        IDebtToken(VARIABLE_DEBT_USDC).approveDelegation(
+            migrateAavePositionsAddress, 
+            type(uint256).max
+        );
+        vm.stopPrank();
+
+        // Start flash loan
+        vm.startPrank(BORROWER);
+        flashloan.migrateAavePositions(
+            RECEIVER, 
+            debtTokenPositions, 
+            aTokenPositions
+        );
+        vm.stopPrank();
+
+        (uint WETH_LENDED_RECEIVER, , , , , , , ,) = protocolDataProvider.getUserReserveData(WETH_ADDRESS, RECEIVER);
+        (uint WBTC_LENDED_RECEIVER, , , , , , , ,) = protocolDataProvider.getUserReserveData(WBTC_ADDRESS, RECEIVER);
+        (uint SNX_LENDED_RECEIVER, , , , , , , ,) = protocolDataProvider.getUserReserveData(SNX_ADDRESS, RECEIVER);
+
+        (, , uint USDC_BORROWED_RECEIVER, , , , , ,) = protocolDataProvider.getUserReserveData(USDC_ADDRESS, RECEIVER);
+
+        // Check lending positions (transferred)
+        assertEq(WETH_LENDED_RECEIVER, WETH_LENDED);
+        assertEq(WBTC_LENDED_RECEIVER, WBTC_LENDED);
+        assertEq(SNX_LENDED_RECEIVER, SNX_LENDED);
+
+        uint256 flashloanFee = USDC_BORROWED * 9 / 10000;
+        assertEq(USDC_BORROWED_RECEIVER, USDC_BORROWED + flashloanFee);
+    }
 }   
